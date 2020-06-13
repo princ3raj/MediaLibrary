6d2e466328ade191dc0bb299f71b03d13f4f905f
princeraj <princ3raj1999@gmail.com>
2020-06-14 05:05:22 +0530

    media library practice site
    first commit

---
+++ inc/functions.php
@@ -0,0 +1,43 @@
+<?php
+function get_item_html($id,$item)
+{
+    $output= "<li><a href='#'><img src='"
+    .$item["img"]."' alt='"
+    .$item["title"]."'/>"
+    ."<p>View Details</p>"
+    ."</a></li>";
+
+    return $output; 
+}
+
+function array_category($catalog,$category)
+{
+
+    if($category==null)
+    {
+        return array_keys($catalog);
+    }
+
+    $output=array();
+
+    foreach($catalog as $id=>$item)
+    {
+        if(strtolower($category)==strtolower($item["category"]))
+        {
+            $sort=$item["title"];
+            $sort=ltrim($sort,"The ");
+            $sort=ltrim($sort,"A ");
+            $sort=ltrim($sort,"An ");
+            $output[$id]=$sort;
+        }
+  
+    }
+
+    asort($output);
+    return array_keys($output);
+}
+ 
+
+
+
+?>
---
+++ suggest.php
@@ -0,0 +1,10 @@
+<?php 
+$section="suggest";
+$pageTitle='Suggest a Media Item';
+include('inc/header.php'); ?>
+
+<div class="section page">
+    <h1>Suggest a media item</h1>
+</div>
+
+<?php include('inc/footer.php');?>

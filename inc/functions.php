6275ca871bb19e1840516ba9b189f2df099859c2
princeraj <princ3raj1999@gmail.com>
2020-06-14 23:32:48 +0530

    details page added
    added details page

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,7 @@
 <?php
 function get_item_html($id,$item)
 {
-    $output= "<li><a href='#'><img src='"
+    $output= "<li><a href='details.php?id=".$id."'><img src='"
     .$item["img"]."' alt='"
     .$item["title"]."'/>"
     ."<p>View Details</p>"
@@ -13,16 +13,13 @@ function get_item_html($id,$item)
 function array_category($catalog,$category)
 {
 
-    if($category==null)
-    {
-        return array_keys($catalog);
-    }
+    
 
     $output=array();
 
-    foreach($catalog as $id=>$item)
+    foreach( $catalog as $id=>$item)
     {
-        if(strtolower($category)==strtolower($item["category"]))
+        if($category==null OR strtolower($category)==strtolower($item["category"]))
         {
             $sort=$item["title"];
             $sort=ltrim($sort,"The ");
